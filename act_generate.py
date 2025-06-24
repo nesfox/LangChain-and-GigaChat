646d276b3e440f3a2a95d8f42b9c540572991805
nesfox <nesfox@mail.ru>
2025-06-24 15:15:31 +0300

    the proven scheme of work of two agents

--- act_generate.py
+++ act_generate.py
@@ -1,22 +1,28 @@
 from dataclasses import dataclass, asdict
 import json
 import os
-import subprocess
+import subprocess # для запуска внешних процессов
 from typing import Sequence
-import uuid
+import uuid # для генерации уникальных идентификаторов
 
 from dotenv import find_dotenv, load_dotenv
+# интерфейс для языковых моделей
 from langchain_core.language_models import LanguageModelLike
+# конфигурация для запускаемых объектов
 from langchain_core.runnables import RunnableConfig
+# базовый класс и декоратор для инструментов
 from langchain_core.tools import BaseTool, tool
 from langchain_gigachat.chat_models import GigaChat
+# создание агента
 from langgraph.prebuilt import create_react_agent
+# сохранение состояния в памяти
 from langgraph.checkpoint.memory import InMemorySaver
 from mail import fetch_recent_emails
 
 
 load_dotenv(find_dotenv())
-PATH_FILE = "C:/Users/smoly/LangChain/LangChain-and-GigaChat/ООО_реквизиты_Маркетсистемс.docx"
+# PATH_FILE = "ООО_реквизиты_Маркетсистемс.docx"
+
 
 @dataclass
 class Bank:
@@ -25,6 +31,8 @@ class Bank:
     current_account: str  # расчётный счёт
     corporate_account: str  # корреспондентский счёт
     BIC: str  # БИК банка
+
+
 @dataclass
 class Customer:
     """Заказчик"""
@@ -34,6 +42,7 @@ class Customer:
     address: str  # юридический адрес
     signatory: str  # подписант
     bank: Bank  # банковские реквизиты заказчика
+
 
 @dataclass
 class Job:
@@ -45,6 +54,7 @@ class Job:
     price_nds: int  # стоимость с НДС
     price_nds_18: int  # стоимость с НДС 18%
     price_nds_10: int  # стоимость с НДС 10%
+
 
 @tool
 def generate_pdf_act(customer: Customer, jobs: list[Job]) -> None:
@@ -59,25 +69,27 @@ def generate_pdf_act(customer: Customer, jobs: list[Job]) -> None:
     Returns:
         None
     """
-    # print(f"generate_pdf_act({asdict(customer)}, {list(map(lambda j: asdict(j), jobs))})")
+    # Преобразуем объекты в словари
     act_json = {
         "customer": asdict(customer),
         "jobs": list(map(
             lambda j: asdict(j), jobs
         ))
     }
-    with open(os.path.join("C:/Users/smoly/LangChain/LangChain-and-GigaChat/typst", "act.json"), "w", encoding="utf-8") as f:
+    # Сохраняем данные в JSON файл
+    with open(os.path.join("C:/Users/smoly/SBER_GIGA/LangChain-and-GigaChat/typst", "act.json"), "w", encoding="utf-8") as f:
         json.dump(act_json, f, ensure_ascii=False)
     command = ["typst", "compile", "--root", "./typst", "typst/act.typ"]
     try:
         subprocess.run(command,
-               check=True,
-               stdout=subprocess.PIPE,
-               stderr=subprocess.PIPE,
-               text=True,
-               encoding="utf-8")
+                       check=True,
+                       stdout=subprocess.PIPE,
+                       stderr=subprocess.PIPE,
+                       text=True,
+                       encoding="utf-8")
     except subprocess.CalledProcessError as e:
         print(e.stderr)
+
 
 @tool
 def generate_pdf_invoice(customer: Customer, jobs: list[Job]) -> None:
@@ -98,7 +110,7 @@ def generate_pdf_invoice(customer: Customer, jobs: list[Job]) -> None:
             lambda j: asdict(j), jobs
         ))
     }
-    with open(os.path.join("C:/Users/smoly/LangChain/LangChain-and-GigaChat/typst", "invoice.json"), "w") as f:
+    with open(os.path.join("C:/Users/smoly/SBER_GIGA/LangChain-and-GigaChat/typst", "invoice.json"), "w", encoding="utf-8") as f:
         json.dump(invoice_json, f, ensure_ascii=False)
     command = ["typst", "compile", "--root", "./typst", "typst/invoice.typ"]
     try:
@@ -110,6 +122,7 @@ def generate_pdf_invoice(customer: Customer, jobs: list[Job]) -> None:
         print(e.stderr)
 
 
+# Класс LLM-агента
 class LLMAgent:
     def __init__(self, model: LanguageModelLike, tools: Sequence[BaseTool]):
         self._model = model
@@ -118,19 +131,20 @@ class LLMAgent:
             tools=tools,
             checkpointer=InMemorySaver())
         self._config: RunnableConfig = {
-                "configurable": {"thread_id": uuid.uuid4().hex}}
+                "configurable": {"thread_id": uuid.uuid4().hex}} # уникальный ID сессии
 
     def upload_file(self, file):
+        """Загрузка файла в GigaChat"""
         file_uploaded_id = self._model.upload_file(file).id_  # type: ignore
         return file_uploaded_id
 
     def invoke(
         self,
         content: str,
-        attachments: list[str]|None=None,
-        temperature: float=0.1
+        attachments: list[str] | None = None,
+        temperature: float = 0.1
     ) -> str:
-        """Отправляет сообщение в чат"""
+        """Отправка сообщения агенту"""
         message: dict = {
             "role": "user",
             "content": content,
@@ -153,15 +167,40 @@ def get_user_prompt() -> str:
 
 
 def main():
+    system_prompt = (
+        "Твоя задача найти файл,  содержащий реквизиты компании в одном из"
+        "писем на почте. Найди имя этого файла и выведи его ответом. В ответе"
+        "только имя файла или слово нет, если не нашёл"
+    )
 
-    # более простой промпт для запуска (пробного)
+    model = GigaChat(
+        model="GigaChat-2-Max",
+        verify_ssl_certs=False,
+    )
+
+    @tool
+    def upload_email_attacnment_file_to_llm(filename: str) -> None:
+        """Загружает конкретный файл-аттачмент конкретного писыма в LLM"""
+        print(f"upload {filename} to LLM")
+        agent.upload_file(open(filename, "rb"))
+
+    agent = LLMAgent(model, tools=[fetch_recent_emails])
+    filename = agent.invoke(content=system_prompt)
+    if filename == "нет":
+        exit("ничего не получилось")
+
+
+    agent = LLMAgent(model, tools=[generate_pdf_act,
+                                   generate_pdf_invoice])
+    file_uploaded_id = agent.upload_file(open(f"attachments/{filename}", "rb"))
+
     system_prompt = (
         "Твоя задача спросить у пользователя, что он хочет сгенерировать — акт или счёт или оба документа. "
         "Затем нужно сгенерировать акт или счёт, для этого тебе надо взять реквизиты "
         "контрагента из приложенного файла, а также запроси работы для включения в "
         "акт (наименования задач и их стоимость), работ может быть несколько. "
         "Если пользователь указывает в качестве работы аренда жилья, то для документов берём одну работу, в точности такую "
-        "\"Аренда жилого помещения «Гостевой дом Kalina»\", стоимостью 5 тыс руб. за сутки и спроси на сколько суток снимают если не указали"
+        "Аренда жилого помещения «Гостевой дом Kalina», стоимостью 5 тыс руб. за сутки и спроси на сколько суток снимают если не указали"
         "Никакие данные не придумывай, всё необходимое строго запроси у "
         "пользователя. Мои реквизиты заказчика не запрашивай, они есть в моём коде. "
         "Имя и отчество подписанта сокращаем до одной первой буквы, "
@@ -171,28 +210,25 @@ def main():
         "ставим »."
     )
 
-
-    model = GigaChat(
-    model="GigaChat-Pro",
-    verify_ssl_certs=False,
-    )
-
-    agent = LLMAgent(model, tools=[generate_pdf_act, generate_pdf_invoice])
-
-
-    file_uploaded_id= agent.upload_file(open(PATH_FILE, "rb"))
     agent_response = agent.invoke(content=system_prompt, attachments=[file_uploaded_id])
 
-    print("Agent response: ", agent_response)
-    while(True):
-        print_agent_response(agent_response)
+    while (True):
+        # ... передача сообщения агенту ...
         agent_response = agent.invoke(get_user_prompt())
+        print_agent_response(agent_response)
+
 
 if __name__ == "__main__":
     try:
         main()
     except KeyboardInterrupt:
         print("/nПока, буду ждать ещё!")
+
+
+
+
+
+
 
 
 
--- typst/invoice.typ
+++ typst/invoice.typ
@@ -2,100 +2,77 @@
 #import "@preview/zero:0.3.3": num, set-group
 #set-group(size: 3, separator: sym.space.thin, threshold: 4)
 
-#let act = json("act.json")
-#let act_sum = act.jobs.map(job => float(job.at("count")) * float(job.at("price"))).sum()
-#let act_nds = act_sum * 0.15  // НДС 18%
-#let act_total = act_sum + act_nds
+#let invoice = json("invoice.json")
 
-= Акт сдачи-приемки выполненных работ (оказания услуг)
+#let invoice_overall_sum = invoice.jobs.map(job => job.at("price") * job.at("count")).sum()
+#let invoice_jobs_count = invoice.jobs.len()
 
-#datetime.today().display("[day].[month].[year]")
+#let get_invoice_number(
+  // d — дата; по умолчанию берём сегодняшнюю
+  num,
+  d: datetime.today()
+) = {
+  d.display(
+    "[year repr:last_two padding:zero]" + // YY
+    "[month padding:zero]"              + // MM
+    "[day padding:zero]"                + // DD
+    num,                                 // суффикс
+  )
+}
+
+#let ru-months = (
+  "января",  "февраля", "марта",     "апреля",
+  "мая",     "июня",    "июля",      "августа",
+  "сентября","октября", "ноября",    "декабря",
+)
+#let today = datetime.today()
+
 
 #table(
-  columns: 2,
-  stroke: none,
-  inset: (left: 0pt),
-  [*Исполнитель:*], [ООО «Мобайл»],
-  [*Заказчик:*],    [#act.customer.at("name")]
+  columns: (auto, 1fr, auto, auto),
+  [ИНН 482423943474], [ОГРНИП 313482405700058], table.cell(rowspan: 2, align: bottom)[Сч. №], table.cell(rowspan: 2, align: bottom)[40802810602230000885],
+  table.cell(colspan: 2)[Получатель\
+  ИП СМОЛЯК ПОЛИНА ВИКТОРОВНА],
+  table.cell(colspan: 2, rowspan: 2)[Банк получателя\
+  АО "СБЕР-БАНК"],
+  table.cell()[БИК],
+  table.cell()[044525593],
+  table.cell()[Сч. №],
+  table.cell()[30101810200000000593],
 )
 
-Мы, представители Исполнителя #text(weight: "bold")[ООО «Мобайл»],
-#linebreak()
-в лице директора Козлова Дмитрия Юрьевича
-#linebreak()
-с одной стороны, и представители Заказчика #text(weight: "bold")[#act.customer.at("name")],
-#linebreak()
-в лице директора #act.customer.at("signatory")
-#linebreak()
-с другой стороны, составили настоящий акт о том, что Исполнителем были выполнены следующие работы (оказаны следующие услуги) по договору №211-17 от 01.06.2017 г.:
-
-#table(
-  columns: 6,
-  [№], [Наименование товара], [Ед. изм.], [Кол-во], [Цена], [Сумма],
-  ..for (index, job) in act.jobs.enumerate() {(
-    [
-      #(index + 1)
-    ], [
-      #job.at("task")
-    ], [
-      #job.at("unit")
-    ], [
-      #job.at("count")
-    ], [
-      #num(float(job.at("price")))
-    ], [
-      #num(float(job.at("count")) * float(job.at("price")))
-    ]
-  )}
-)
-
-#align(right)[
-  #text(weight: "bold")[Итого:] #num(act_sum)
-  #linebreak()
-  #text(weight: "bold")[В том числе НДС (15%):] #num(act_nds)
-  #linebreak()
-  #text(weight: "bold")[Всего (с учетом НДС):] #num(act_total)
+#set align(center)
+#block(below: 1.5em, above: 2em)[
+#align(center)[
+= СЧЕТ № #get_invoice_number("01") от #today.day()~#ru-months.at(today.month() - 1)~#today.year() г.
+]
 ]
 
-Всего оказано услуг на сумму (с НДС): #text(weight: "bold")[#ru-words(int(act_total))] рублей 00 коп.
-#linebreak()
-в т.ч. НДС — #text(weight: "bold")[#ru-words(int(act_nds))] рублей 00 коп.
+#set align(left)
 
-Вышеперечисленные работы (услуги) выполнены полностью и в срок. Заказчик претензий по объему, качеству и срокам оказания услуг не имеет.
+Плательщик: #invoice.customer.name ИНН #invoice.customer.INN ОГРН #invoice.customer.INN
 
 #table(
-  columns: 2,
-  stroke: none,
-  inset: (left: 0pt),
-  [
-    ЗАКАЗЧИК\
-    #act.customer.at("name")\
-    ИНН #act.customer.at("INN") КПП #act.customer.at("KPP")\
-    Адрес: #act.customer.at("address")\
-    Р/с #act.customer.at("bank").at("current_account")\
-    К/с #act.customer.at("bank").at("corporate_account")\
-    Банк #act.customer.at("bank").at("name")\
-    БИК #act.customer.at("bank").at("BIC")\
-    Директор #act.customer.at("signatory")\
-    (подпись)\
-    М.П.
-  ], [
-    ИСПОЛНИТЕЛЬ\
-    ООО «Мобайл»\
-    ИНН 9876543210 КПП 12345678\
-    Адрес: г. Москва, ул. Серова, 25\
-    Р/с 12345678901234567890\
-    К/с 123456789012\
-    Банк Гаммабанк\
-    БИК 74569882\
-    Директор Д.Ю. Козлов\
-    (подпись)\
-    М.П.
-  ],[
-    \
-    #text("________________/") #act.customer.at("signatory") /
-  ], [
-    \
-    #text("________________/") Козлов Д.Ю. /
-  ]
+  columns: (0.4fr, 2.7fr, 0.7fr, 0.7fr, 0.8fr, 1.3fr),
+  table.cell(align: center)[№], [Наименование товара], table.cell(align: center)[Ед. изм.], table.cell(align: center)[Кол-во], table.cell(align: center)[Цена, ₽], table.cell(align: center)[Сумма, ₽],
+  ..for (index, job) in invoice.jobs.enumerate() {(
+    table.cell(align: center)[
+      #(index + 1)
+    ],[
+      #job.at("task")
+    ], table.cell(align: center)[шт], table.cell(align: center)[#num(job.at("count"))], table.cell(align: center)[
+      #align(center)[#num(job.at("price"))]
+    ],table.cell(align: center)[
+      #align(center)[#num(invoice_overall_sum)]
+    ]
+  )},
+  table.cell(colspan: 5, stroke: none, align: right)[*Итого:*], table.cell(align: center)[#num(invoice_overall_sum)],
+  table.cell(colspan: 5, stroke: none, align: right)[*НДС:*], table.cell(align: center)[не облагается],
+  table.cell(colspan: 5, stroke: none, align: right)[*Всего к оплате:*], table.cell(align: center)[#num(invoice_overall_sum)],
 )
+
+Всего наименований #invoice_jobs_count , на сумму #num(invoice_overall_sum) (#ru-words(invoice_overall_sum)) руб ноль копеек. Оплачивая настоящий счёт, вы присоединяетесь к Оферте, опубликованной на сайте https://to.digital/oferta.pdf.
+
+#block(above: 3em)[
+  ИП Смоляк #text("__________________________") Смоляк П.В.
+]
